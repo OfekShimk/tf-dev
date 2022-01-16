fe6074b4cf40be07127ab9e4fe8b9c744e0e1a5d
LironElbaz <lelbaz@paloaltonetworks.com>
2022-01-16 17:14:12 +0200

    adding yor tag

--- static-web-site/main.tf
+++ static-web-site/main.tf
@@ -8,8 +8,16 @@ resource "aws_s3_bucket" "this" {
   }
 
   tags = {
-    "Managed via" = "terraform"
-    Environment = "training"
+    "Managed via"        = "terraform"
+    Environment          = "training"
+    git_commit           = "fd646f38c0eda7ce0ab52a4d87493b79e2512474"
+    git_file             = "static-web-site/main.tf"
+    git_last_modified_at = "2021-08-25 12:13:10"
+    git_last_modified_by = "lelbaz@paloaltonetworks.com"
+    git_modifiers        = "lelbaz"
+    git_org              = "LironElbaz"
+    git_repo             = "tf-dev"
+    yor_trace            = "e7451a11-6895-417e-b54c-5802399f107d"
   }
 }
 
@@ -22,8 +30,16 @@ resource "aws_s3_bucket_object" "this" {
   etag = filemd5(var.index_path)
 
   tags = {
-    "Managed via" = "terraform"
-    Environment = "training"
+    "Managed via"        = "terraform"
+    Environment          = "training"
+    git_commit           = "fd646f38c0eda7ce0ab52a4d87493b79e2512474"
+    git_file             = "static-web-site/main.tf"
+    git_last_modified_at = "2021-08-25 12:13:10"
+    git_last_modified_by = "lelbaz@paloaltonetworks.com"
+    git_modifiers        = "lelbaz"
+    git_org              = "LironElbaz"
+    git_repo             = "tf-dev"
+    yor_trace            = "c4dc6dfc-1562-42cc-ba9c-30b6e78b1cfc"
   }
 }
 
@@ -31,14 +47,14 @@ resource "aws_s3_bucket_policy" "this" {
   bucket = aws_s3_bucket.this.id
 
   policy = jsonencode({
-    Version   = "2012-10-17"
+    Version = "2012-10-17"
     Statement = [
       {
         Sid       = "PublicReadGetObject"
         Effect    = "Allow"
         Principal = "*"
         Action    = "s3:GetObject"
-        Resource  = [
+        Resource = [
           aws_s3_bucket.this.arn,
           "${aws_s3_bucket.this.arn}/*"
         ]
