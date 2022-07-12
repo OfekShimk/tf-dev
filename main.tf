ef6259917b2d6072450a3ee6ccf68d039f0750ba
OfekShimk <70918223+OfekShimk@users.noreply.github.com>
2022-07-12 15:50:47 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-  region = "eu-west-1"
+  region = "us-west-2"
   profile = "dev3"
 }
 
 module "static_web_site" {
   source        = "./static-web-site"
-
+  
   index_path    = "index.html"
-  bucket_name   = "lirons-bucket"
+  bucket_name   = "shimko-bucket"
 }
